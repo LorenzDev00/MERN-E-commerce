9ed1615d173fe8bc92f8b2798835d346854a2df3
Lorenzo Bignami <95931734+LorenzDev00@users.noreply.github.com>
2026-06-30 17:21:59 +0200

    Update ProtectedRoute.js

--- frontend/src/components/ProtectedRoute.js
+++ frontend/src/components/ProtectedRoute.js
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import { Navigate } from 'react-router-dom';
 import { Store } from '../screens/Store';
 
+// Route guard: renders children only if the user is logged in
 export default function ProtectedRoute({ children }) {
   const { state } = useContext(Store);
   const { userInfo } = state;
